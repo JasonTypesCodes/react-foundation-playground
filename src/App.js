3d0f8d5e8de753022d95f6e9832ea742135c8ac4
Jason Schindler <jason@types.codes>
2018-03-07 23:33:29 -0600

    Part of a modal with a title

--- src/App.js
+++ src/App.js
@@ -12,6 +12,25 @@ class App extends Component {
       <FoundationApp className="App AnotherClass">
         <NavBar />
 
+        <div className="row">
+          <div className="columns">
+            <p><a data-open="exampleModal1">Click me for a modal</a></p>
+
+            <div className="reveal" id="exampleModal1" data-reveal="" style={{padding: 0}}>
+              <div className="card">
+                <div className="card-divider alert">
+                  <h5>A Title</h5>
+                  <button className="close-button" data-close="" aria-label="Close reveal" type="button">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div className="card-section">Some content</div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
         <div>
           Other Stuff
         </div>
